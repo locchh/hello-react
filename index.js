6a51b4af5c8dd1de1af817f495a2c923c2f54793
LocCH <locc7196@gmail.com>
2026-05-16 11:12:19 +0700

    feat: set up basic React app with CSS styling

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+// Root component
+function App() {
+    return React.createElement('h1', null, 'Hello, React!');
+}
+
+// Mount App into #root
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(React.createElement('h1', null, 'Hello from React!'));
+root.render(React.createElement(App));
